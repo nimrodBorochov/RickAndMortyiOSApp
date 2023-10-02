192fc14c1f61816e182a3edbc8199cf7e15c624f
nimrodBorochov <nimrodBorochov@gmail.com>
2023-10-02 23:01:13 +0300

    Create API Request

--- RickAndMorty/APIClient/RMEndpoint.swift
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  RMEndPoint.swift
+//  RMEndpoint.swift
 //  RickAndMorty
 //
 //  Created by Nimrod Borochov on 02/10/2023.
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Represents unique API endpoints
-@frozen enum RMEndPoint: String {
+@frozen enum RMEndpoint: String {
     /// Endpoint to get character info
     case character // "character"
     /// Endpoint to get location info
