b97fb79abd82f64b6053157d8c573faa41f4ce45
nimrodBorochov <nimrodBorochov@gmail.com>
2023-10-03 15:58:50 +0300

    Create Cell and CellModel

---
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Nimrod Borochov on 03/10/2023.
+//
+
+import Foundation
+
+final class RMCharacterCollectionViewCellViewModel {
+    public let characterName: String
+    private let characterStatus: RMCharacterStatus
+    private let characterImageUrl: URL?
+
+    // MARK: - Init
+
+    init(characterName: String, characterStatus: RMCharacterStatus, characterImageUrl: URL?) {
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterImageUrl = characterImageUrl
+    }
+
+    public var characterStatusText: String {
+        characterStatus.rawValue
+    }
+
+    public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = characterImageUrl else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+
+        let request = URLRequest(url: url)
+        // TODO: Abstract to Image Manager
+        // TODO: check if change task to async
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+
+            completion(.success(data))
+        }
+        task.resume()
+
+
+
+    }
+}
